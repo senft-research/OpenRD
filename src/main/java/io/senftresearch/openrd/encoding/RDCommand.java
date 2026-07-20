4b5b4b25ba3a555451c4a5bb0bb404594843af8c
senft-research <JordanSenft@proton.me>
2026-07-20 11:05:39 +0100

    Removed unneeded private from constructor of RDCommand.java

--- src/main/java/io/senftresearch/openrd/encoding/RDCommand.java
+++ src/main/java/io/senftresearch/openrd/encoding/RDCommand.java
@@ -2,7 +2,7 @@ package io.senftresearch.openrd.encoding;
 
 public enum RDCommand {
     PART_INIT("-b-", "ca 22 %s e7 54 00 00 00 00 00 00 e7 54 01 00 00 00");
-    private RDCommand(String format, String encoding){
+    RDCommand(String format, String encoding){
         this.format = format;
         this.encoding = encoding;
     }
