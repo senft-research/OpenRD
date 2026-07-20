fbf5254f2293935667edaed6743a34939fb8a4e6
senft-research <JordanSenft@proton.me>
2026-07-20 12:01:44 +0100

    Formatting changes to RDCommand.java

--- src/main/java/io/senftresearch/openrd/encoding/RDCommand.java
+++ src/main/java/io/senftresearch/openrd/encoding/RDCommand.java
@@ -2,10 +2,12 @@ package io.senftresearch.openrd.encoding;
 
 public enum RDCommand {
     PART_INIT("-b-", "ca 22 %s e7 54 00 00 00 00 00 00 e7 54 01 00 00 00");
+
     RDCommand(String format, String encoding){
         this.format = format;
         this.encoding = encoding;
     }
+
     private String format;
     private String encoding;
 
