5cbf6a2e8042e851f0e94d136ffa89e1cd5ea5d7
senft-research <JordanSenft@proton.me>
2026-07-27 11:41:06 +0100

    Populated RDPartPointsCommand.java and fixed a missed RDCommandArg (for layer number) in RDPartPointsCommand.getArgs.

--- src/main/java/io/senftresearch/openrd/encoding/commands/types/part/RDPartPointsExCommand.java
+++ src/main/java/io/senftresearch/openrd/encoding/commands/types/part/RDPartPointsExCommand.java
@@ -3,15 +3,14 @@ package io.senftresearch.openrd.encoding.commands.types.part;
 import io.senftresearch.openrd.RDBoundingBox;
 import io.senftresearch.openrd.encoding.RDEncodingType;
 import io.senftresearch.openrd.encoding.commands.AbstractRDCommand;
-import io.senftresearch.openrd.encoding.commands.RDCommand;
 import io.senftresearch.openrd.encoding.commands.RDCommandHex;
 import io.senftresearch.openrd.encoding.commands.types.process.RDCommandArg;
 
-public class RDPartPointsCommand extends AbstractRDCommand {
-    private RDBoundingBox boundingBox;
-    private int layerNumber;
+public class RDPartPointsExCommand extends AbstractRDCommand {
+    private final RDBoundingBox boundingBox;
+    private final int layerNumber;
 
-    public RDPartPointsCommand(RDBoundingBox boundingBox, int layerNumber){
+    public RDPartPointsExCommand(RDBoundingBox boundingBox, int layerNumber){
         this.boundingBox = boundingBox;
         this.layerNumber = layerNumber;
     }
@@ -19,11 +18,11 @@ public class RDPartPointsCommand extends AbstractRDCommand {
     @Override
     public RDCommandArg[] getArgs() {
         return new RDCommandArg[]{
-                new RDCommandArg(RDCommandHex.RD_PART_MIN_POINT.getHexCode(), RDEncodingType.HEX),
+                new RDCommandArg(RDCommandHex.RD_PART_MIN_POINT_EX.getHexCode(), RDEncodingType.HEX),
                 new RDCommandArg(this.layerNumber, RDEncodingType.BYTE),
                 new RDCommandArg(this.boundingBox.topLeft().x(), RDEncodingType.NUMBER),
                 new RDCommandArg(this.boundingBox.topLeft().y(), RDEncodingType.NUMBER),
-                new RDCommandArg(RDCommandHex.RD_PART_MAX_POINT.getHexCode(), RDEncodingType.HEX),
+                new RDCommandArg(RDCommandHex.RD_PART_MAX_POINT_EX.getHexCode(), RDEncodingType.HEX),
                 new RDCommandArg(this.layerNumber, RDEncodingType.BYTE),
                 new RDCommandArg(this.boundingBox.bottomRight().x(), RDEncodingType.NUMBER),
                 new RDCommandArg(this.boundingBox.bottomRight().y(), RDEncodingType.NUMBER)
