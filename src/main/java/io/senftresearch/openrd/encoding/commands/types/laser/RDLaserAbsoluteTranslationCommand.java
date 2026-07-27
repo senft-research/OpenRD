8b720c4c627c56c9e255e688c055ebe985fba5a6
senft-research <JordanSenft@proton.me>
2026-07-27 15:14:22 +0100

    Added RDLaserAbsoluteTranslationCommand.java (not implemented yet).

---
+++ src/main/java/io/senftresearch/openrd/encoding/commands/types/laser/RDLaserAbsoluteTranslationCommand.java
@@ -0,0 +1,29 @@
+package io.senftresearch.openrd.encoding.commands.types.laser;
+
+import io.senftresearch.openrd.encoding.RDEncodingType;
+import io.senftresearch.openrd.encoding.commands.AbstractRDCommand;
+import io.senftresearch.openrd.encoding.commands.RDCommandHex;
+import io.senftresearch.openrd.encoding.commands.types.process.RDCommandArg;
+
+public class RDLaserAbsoluteTranslationCommand extends AbstractRDCommand {
+
+    private final int xCoord;
+    private final int yCoord;
+    private final RDCommandHex commandHex;
+
+
+    public RDLaserAbsoluteTranslationCommand(int xCoord, int yCoord, boolean isTravel){
+        commandHex = isTravel ? RDCommandHex.RD_ABSOLUTE_MOVE : RDCommandHex.RD_ABSOLUTE_CUT;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+    }
+
+    @Override
+    public RDCommandArg[] getArgs() {
+        return new RDCommandArg[]{
+                new RDCommandArg(commandHex.getHexCode(), RDEncodingType.HEX),
+                new RDCommandArg(xCoord, RDEncodingType.REL_COORD),
+                new RDCommandArg(yCoord, RDEncodingType.REL_COORD)
+        };
+    }
+}
