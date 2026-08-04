e828d55dee4f293401476dcf1c128adf058b5dfa
senft-research <JordanSenft@proton.me>
2026-08-04 12:38:06 +0100

    Populated Javadocs for RDBoundingBox.java

--- src/main/java/io/senftresearch/openrd/RDBoundingBox.java
+++ src/main/java/io/senftresearch/openrd/RDBoundingBox.java
@@ -1,4 +1,10 @@
 package io.senftresearch.openrd;
 
+/**
+ * The boundaries of a specific "job" (represented by a box that is defined by the x,y coordinates of the box's top left
+ * and bottom right vertices.
+ * @param topLeft The top left vertex of the box.
+ * @param bottomRight The bottom right vertex of the box.
+ */
 public record RDBoundingBox(RDPoint topLeft, RDPoint bottomRight) {
 }
