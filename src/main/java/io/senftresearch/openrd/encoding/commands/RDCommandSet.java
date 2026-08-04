9bb4d5264c7ed4e86362bf455ea4b1ae259ddaef
senft-research <JordanSenft@proton.me>
2026-08-04 12:39:35 +0100

    Populated Javadocs for RDCommandSet.java

--- src/main/java/io/senftresearch/openrd/encoding/commands/RDCommandSet.java
+++ src/main/java/io/senftresearch/openrd/encoding/commands/RDCommandSet.java
@@ -3,9 +3,16 @@ package io.senftresearch.openrd.encoding.commands;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A set of {@linkplain RDCommand Commands} that can be written to the {@linkplain io.senftresearch.openrd.encoding.RDEncoder Encoder}
+ * simultaneously (allowing for a more controlled order for the commands to be written).
+ */
 public class RDCommandSet {
     private List<RDCommand> commands = new ArrayList<>();
 
+    /**
+     * Private constructor, to ensure the builder pattern is utilised.
+     */
     private RDCommandSet(){}
 
     public List<RDCommand> getCommands() {
